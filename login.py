878d9b7c9baef0c120b7133435fae9cf8b86bfec
Lin kong <3199608937@qq.com>
2023-06-21 00:38:28 +0800

    first commot

---
+++ login.py
@@ -0,0 +1,105 @@
+import os
+import platform
+from plyer import notification
+import requests
+import json
+
+
+def sendCode(phone):
+    url = "https://userapi.qiekj.com/common/sms/sendCode"
+
+    payload = f"phone={phone}&template=reg"
+    headers = {
+        'Host': 'userapi.qiekj.com',
+        'channel': 'ios_app',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'Cookie': 'SERVERID=f2b5f835c45a6ca30a9d3ff318bef498|1687271707|1687271654; '
+                  'acw_tc=781bad3416872716540207737e51e812c531a8ba6c0c4f4ce9c92f7c6340ef; '
+                  'SERVERID=f2b5f835c45a6ca30a9d3ff318bef498|1687272051|1687271654',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'Version': '1.28.2',
+        'User-Agent': 'QEUser/1.28.2 (com.qiekj.QEUser; build:2; iOS 16.5.0) Alamofire/5.6.4',
+        'Accept-Language': 'zh-Hant-HK;q=1.0, zh-Hans-CN;q=0.9, en-CN;q=0.8, en-GB;q=0.7, ja-CN;q=0.6, '
+                           'yue-Hant-CN;q=0.5',
+        'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+        'Content-Length': '30'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    state = json.loads(response.text)['msg']
+    if state == '成功':
+        return True
+    else:
+        return False
+
+
+def verifyCode(phone, code):
+    # 成功即刻返回token
+    url = "https://userapi.qiekj.com/user/reg"
+
+    payload = f"channel=ios_app&phone={phone}&verify={code}"
+    headers = {
+        'Host': 'userapi.qiekj.com',
+        'channel': 'ios_app',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'Cookie': 'SERVERID=f2b5f835c45a6ca30a9d3ff318bef498|1687271750|1687271654; '
+                  'acw_tc=781bad3416872716540207737e51e812c531a8ba6c0c4f4ce9c92f7c6340ef; '
+                  'SERVERID=f2b5f835c45a6ca30a9d3ff318bef498|1687272550|1687271654',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'Version': '1.28.2',
+        'User-Agent': 'QEUser/1.28.2 (com.qiekj.QEUser; build:2; iOS 16.5.0) Alamofire/5.6.4',
+        'Accept-Language': 'zh-Hant-HK;q=1.0, zh-Hans-CN;q=0.9, en-CN;q=0.8, en-GB;q=0.7, ja-CN;q=0.6, '
+                           'yue-Hant-CN;q=0.5',
+        'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+        'Content-Length': '45'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    result = json.loads(response.text)
+    state = json.loads(response.text)['msg']
+    if state == '成功':
+        tk = result['data']['token']
+        return [True, tk]
+    else:
+        return [False, 'error']
+
+
+def getMacState(t, tk, ):
+    result = []
+    url = "https://userapi.qiekj.com/machineModel/near/machines"
+    machines = ['c9892cb4-bd78-40f6-83c2-ba73383b090a', '4a245cde-538b-47d8-aa35-dd4a28c0e901']
+    payload = f"goodsPage=1&machineTypeId={machines[t]}&pageSize=20&shopId=201901021533390000082157&token={tk}"
+    headers = {
+        "Host": "userapi.qiekj.com",
+        "channel": "ios_app",
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        "Cookie": "SERVERID=40512ddb9a12b9ada666a84a5b904e8b|1687262213|1687262096; "
+                  "acw_tc=781bad3716872606990245450e4632350e42cef70895d1776d5c985de95ec7; "
+                  "SERVERID=40512ddb9a12b9ada666a84a5b904e8b|1687262479|1687262096",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+        "Version": "1.28.2",
+        "User-Agent": "QEUser/1.28.2 (com.qiekj.QEUser; build:2; iOS 16.5.0) Alamofire/5.6.4",
+        "Accept-Language": "zh-Hant-HK;q=1.0, zh-Hans-CN;q=0.9, en-CN;q=0.8, en-GB;q=0.7, ja-CN;q=0.6, "
+                           "yue-Hant-CN;q=0.5",
+        "Accept-Encoding": "br;q=1.0, gzip;q=0.9, deflate;q=0.8",
+        "Content-Length": "145"
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    jsonroot = json.loads(response.text)['data']['items']
+    for item in jsonroot:
+        result.append(item['name'])
+    return result
+
+
+def show_notification(title, text):
+    sysstr = platform.system()
+    if sysstr == "Darwin":
+        os.system(
+            """osascript -e 'display notification "{}" with title "{}"'""".format(text, title))
+    else:
+        notification.notify(
+            title=title,
+            message=text,
+            timeout=10, )
---
+++ main.py
@@ -0,0 +1,61 @@
+import json
+import time
+import login
+import os
+
+
+def start(add=False):
+    if len(os.listdir('./user/')) == 0 or add:
+        print("请输入手机号")
+        phone = input(">> ")
+        if login.sendCode(phone):
+            print('我们向你的手机号发送了一个验证码，请输入')
+            vcode = input(">> ")
+            if login.verifyCode(phone, vcode)[0]:
+                print('登录成功！')
+                tk = login.verifyCode(phone, vcode)[1]
+                # 导入配置
+                json_import = json.loads('{"phone": "' + phone + '", "token": "' + tk + '","delay": 60}')
+                with open(f'./user/{phone}.json', 'wt') as f:
+                    json.dump(json_import, f)
+            else:
+                print('检查你的验证码是否输入正确')
+        else:
+            print('可能出错了')
+    else:
+        print('你当前可以选用的账号如下（输入数字选择）-1新增')
+        userlist = os.listdir('./user/')
+        for i in range(len(userlist)):
+            print(f'[{i}] {userlist[i]}')
+        select = input('>> ')
+        if select == '-1':
+            start(True)
+        else:
+            print('选择成功！！！')
+            with open(f'./user/{userlist[int(select)]}', 'r') as f:
+                config = json.load(f)
+            token = config['token']
+            delay = config['delay']
+            print("请选择你需要蹲的机器类型")
+            print('[0] 洗衣机')
+            print('[1] 烘干机')
+            types = int(input('>> '))
+            mctype = ['洗衣机', '烘干机']
+            print('选择成功！')
+            print('请输入你想要蹲几号楼的机器，数字即可')
+            lh = f"城院{input('>> ')}号楼"
+            print('蹲蹲已经开始蹲啦，届时会有消息提示，请注意查看')
+            flag = True
+            while flag:
+                data = login.getMacState(types, token)
+                for i in data:
+                    if lh in i:
+                        print(i)
+                        login.show_notification('蹲蹲机', '成功蹲到了 ' + lh + ' 的 ' + mctype[types] + ' 请尽快前往！')
+                        flag = False
+                time.sleep(delay)
+
+
+if __name__ == '__main__':
+    print("欢迎使用 蹲蹲小助手 帮你解决烦恼")
+    start()
